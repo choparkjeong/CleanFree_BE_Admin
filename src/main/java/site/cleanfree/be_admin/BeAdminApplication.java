0524b1855c3757354c72b70b2712663498037921
ddolboghi <rhdfyd128@gmail.com>
2024-07-23 00:56:57 +0900

    feat: spring cloud config annotation 추가

--- src/main/java/site/cleanfree/be_admin/BeAdminApplication.java
+++ src/main/java/site/cleanfree/be_admin/BeAdminApplication.java
@@ -2,8 +2,10 @@ package site.cleanfree.be_admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class BeAdminApplication {
 
     public static void main(String[] args) {
