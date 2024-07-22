3579621c1b056423e8d86f1e64abf41cddacb88c
ddolboghi <rhdfyd128@gmail.com>
2024-07-22 15:47:22 +0900

    프로젝트 생성

---
+++ src/test/java/site/cleanfree/be_admin/BeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package site.cleanfree.be_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
