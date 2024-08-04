abc894870d2bd1975763a00b68a01265bbd294eb
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-08-04 15:57:08 +0900

    feat: 몽고DB 접근해서 질문 리스트 조회

--- src/main/java/site/cleanfree/be_admin/recommendation/application/RecommendationService.java
+++ src/main/java/site/cleanfree/be_admin/recommendation/application/RecommendationService.java
@@ -3,11 +3,16 @@ package site.cleanfree.be_admin.recommendation.application;
 
 import site.cleanfree.be_admin.common.BaseResponse;
 import site.cleanfree.be_admin.recommendation.data.dto.ResultResponseDto;
+import site.cleanfree.be_admin.recommendation.data.vo.GetRecommendationResponseVo;
 import site.cleanfree.be_admin.recommendation.data.vo.RecommendRequestVo;
+
+import java.util.List;
 
 public interface RecommendationService {
 
     void recommend(String id, RecommendRequestVo recommendRequestVo);
 
     BaseResponse<ResultResponseDto> getResult(String authorization, String id);
+
+    List<GetRecommendationResponseVo> getRecommendation();
 }
