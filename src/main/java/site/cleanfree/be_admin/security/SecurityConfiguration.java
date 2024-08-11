16c0d84fb36da1f27c5382b8a273ee18a3797dd8
ddolboghi <rhdfyd128@gmail.com>
2024-08-11 22:01:54 +0900

    fix: uri 수정

--- src/main/java/site/cleanfree/be_admin/security/SecurityConfiguration.java
+++ src/main/java/site/cleanfree/be_admin/security/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
                     // 허용 범위
                     .requestMatchers("https://cleanfree.store", "/api/v1/admin/auth/signIn",
                         "/swagger-ui/**", "/v3/api-docs/**", "/health-check",
-                        "/api/v1/googlesheet").permitAll()
+                        "/api/v1/googlesheet/**").permitAll()
                     .anyRequest()
                     .authenticated()
             )
