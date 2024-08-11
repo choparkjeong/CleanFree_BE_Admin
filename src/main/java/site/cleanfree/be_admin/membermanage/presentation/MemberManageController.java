0f5bd1325c17eee13df33b8fb4844aebdc8559b9
ddolboghi <rhdfyd128@gmail.com>
2024-08-12 00:32:34 +0900

    refactor: google app script cors 설정 및 미사용 API 주석 처리

--- src/main/java/site/cleanfree/be_admin/membermanage/presentation/MemberManageController.java
+++ src/main/java/site/cleanfree/be_admin/membermanage/presentation/MemberManageController.java
@@ -1,31 +1,31 @@
-package site.cleanfree.be_admin.membermanage.presentation;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import site.cleanfree.be_admin.common.BaseResponse;
-import site.cleanfree.be_admin.membermanage.application.MemberManageService;
-import site.cleanfree.be_admin.membermanage.dto.FirstAndLastQuestionTime;
-
-@RestController
-@RequestMapping("/api/v1/admin/member-manage")
-@RequiredArgsConstructor
-@Tag(name = "회원 관리 API", description = "회원 관리 API")
-public class MemberManageController {
-
-    private final MemberManageService memberManageService;
-
-    @GetMapping("/all")
-    @Operation(summary = "회원 목록 조회 API", description = "모든 회원 정보를 조회합니다.")
-    public ResponseEntity<BaseResponse<List<FirstAndLastQuestionTime>>> getAllMemberInfos(
-        @RequestHeader String Authorization
-    ) {
-        return ResponseEntity.ok(memberManageService.getMemberInfos());
-    }
-}
+//package site.cleanfree.be_admin.membermanage.presentation;
+//
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import java.util.List;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestHeader;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import site.cleanfree.be_admin.common.BaseResponse;
+//import site.cleanfree.be_admin.membermanage.application.MemberManageService;
+//import site.cleanfree.be_admin.membermanage.dto.FirstAndLastQuestionTime;
+//
+//@RestController
+//@RequestMapping("/api/v1/admin/member-manage")
+//@RequiredArgsConstructor
+//@Tag(name = "회원 관리 API", description = "회원 관리 API")
+//public class MemberManageController {
+//
+//    private final MemberManageService memberManageService;
+//
+//    @GetMapping("/all")
+//    @Operation(summary = "회원 목록 조회 API", description = "모든 회원 정보를 조회합니다.")
+//    public ResponseEntity<BaseResponse<List<FirstAndLastQuestionTime>>> getAllMemberInfos(
+//        @RequestHeader String Authorization
+//    ) {
+//        return ResponseEntity.ok(memberManageService.getMemberInfos());
+//    }
+//}
--- src/main/java/site/cleanfree/be_admin/recommendation/presentation/RecommendationController.java
+++ src/main/java/site/cleanfree/be_admin/recommendation/presentation/RecommendationController.java
@@ -1,52 +1,52 @@
-package site.cleanfree.be_admin.recommendation.presentation;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import site.cleanfree.be_admin.common.BaseResponse;
-import site.cleanfree.be_admin.recommendation.application.RecommendationService;
-import site.cleanfree.be_admin.recommendation.data.dto.ResultResponseDto;
-import site.cleanfree.be_admin.recommendation.data.vo.GetRecommendationResponseVo;
-import site.cleanfree.be_admin.recommendation.data.vo.RecommendRequestVo;
-
-import java.util.List;
-
-@RestController
-@Slf4j
-@RequiredArgsConstructor
-@Tag(name = "맞춤 화장품 답변 API", description = "맞춤 화장품 답변 API")
-@RequestMapping("/api/v1/admin/recommend")
-public class RecommendationController {
-
-    private final RecommendationService recommendationService;
-
-    @PostMapping("/{id}")
-    public ResponseEntity<BaseResponse<?>> recommend(
-        @RequestHeader String Authorization,
-        @PathVariable String id,
-        @RequestBody RecommendRequestVo recommendRequestVo
-    ) {
-        recommendationService.recommend(id, recommendRequestVo);
-        return ResponseEntity.ok()
-                .body(BaseResponse.successResponse("Recommend Success"));
-    }
-
-    @GetMapping("/result/{id}")
-    public ResponseEntity<BaseResponse<ResultResponseDto>> getSearchResult(
-            @RequestHeader String Authorization,
-            @PathVariable String id
-    ) {
-        return ResponseEntity.ok(recommendationService.getResult(Authorization, id));
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<BaseResponse<List<GetRecommendationResponseVo>>> getRecommendation(
-            @RequestHeader String Authorization
-    ) {
-        return ResponseEntity.ok()
-                .body(BaseResponse.successResponse(
-                        "Recommendation Search Success", recommendationService.getRecommendation()));
-    }
-}
+//package site.cleanfree.be_admin.recommendation.presentation;
+//
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import site.cleanfree.be_admin.common.BaseResponse;
+//import site.cleanfree.be_admin.recommendation.application.RecommendationService;
+//import site.cleanfree.be_admin.recommendation.data.dto.ResultResponseDto;
+//import site.cleanfree.be_admin.recommendation.data.vo.GetRecommendationResponseVo;
+//import site.cleanfree.be_admin.recommendation.data.vo.RecommendRequestVo;
+//
+//import java.util.List;
+//
+//@RestController
+//@Slf4j
+//@RequiredArgsConstructor
+//@Tag(name = "맞춤 화장품 답변 API", description = "맞춤 화장품 답변 API")
+//@RequestMapping("/api/v1/admin/recommend")
+//public class RecommendationController {
+//
+//    private final RecommendationService recommendationService;
+//
+//    @PostMapping("/{id}")
+//    public ResponseEntity<BaseResponse<?>> recommend(
+//        @RequestHeader String Authorization,
+//        @PathVariable String id,
+//        @RequestBody RecommendRequestVo recommendRequestVo
+//    ) {
+//        recommendationService.recommend(id, recommendRequestVo);
+//        return ResponseEntity.ok()
+//                .body(BaseResponse.successResponse("Recommend Success"));
+//    }
+//
+//    @GetMapping("/result/{id}")
+//    public ResponseEntity<BaseResponse<ResultResponseDto>> getSearchResult(
+//            @RequestHeader String Authorization,
+//            @PathVariable String id
+//    ) {
+//        return ResponseEntity.ok(recommendationService.getResult(Authorization, id));
+//    }
+//
+//    @GetMapping("/search")
+//    public ResponseEntity<BaseResponse<List<GetRecommendationResponseVo>>> getRecommendation(
+//            @RequestHeader String Authorization
+//    ) {
+//        return ResponseEntity.ok()
+//                .body(BaseResponse.successResponse(
+//                        "Recommendation Search Success", recommendationService.getRecommendation()));
+//    }
+//}
